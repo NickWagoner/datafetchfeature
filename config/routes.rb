4ba223dfce6a6534f28c45ca6eed13c6cf145a0e
Nicholas J. Wagoner <nicholaswagoner@yahoo.com>
2013-11-23 02:33:58 -0600

    Created first panel and drop-down menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Datafetchfeature::Application.routes.draw do
+  root "page#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
